d1d6699fac387193eece70ae6a280fd0e324848f
Sai Reddy <sai6221@gmail.com>
2023-07-11 15:57:04 -0700

    Home Screen Updated.

--- src/Components/MainScreen/IntroBody.jsx
+++ src/Components/MainScreen/IntroBody.jsx
@@ -8,11 +8,19 @@ function IntroBody() {
 
     const [typeWords] = useTypewriter({ words: ["Saieswar Reddy Vaka", "a Machine Learning Enthusiast","a Software Developer", "an Open Source Contributor"], loop: {}, typeSpeed: 50, deleteSpeed: 50, });
   return (
-    <div><div className="portfolio">
+    <div className="introBodyContainer"><div className="portfolio">
     <text className="portfolio-title">Hi There!</text>
     <text className="portfolio-subtitle">I'm <span className = "portfolio-subsubtitle">{typeWords}</span><Cursor></Cursor></text>
     <text className="portfolio-title">based in San Jose, California. <br></br>saieswarreddy.vaka@sjsu.edu</text>
   </div>
+  <div className="imageContainer">
+  <div className="column mt-6">
+          <img
+            className="image has-image-centered vert-move mt-4"
+            src="/Me.jpg"
+          />
+    </div>      
+</div>
   <div className="arrowContainer" >
         <a href="#features"
           ><i
