d54e0ff8d3af42058e8da92103f4136b53d43ac0
Sai Reddy <sai6221@gmail.com>
2023-07-11 14:56:27 -0700

    Added arrow Social Links and Basic Info to the main screen.

--- src/Components/MainScreen/FooterBody.jsx
+++ src/Components/MainScreen/FooterBody.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
+import '/src/css/footerBody.css'
 
 function FooterBody() {
   return (
-    <div>FooterBody</div>
+    <div className="footerContainer">
+       <div className="socialLinks">
+        <a></a>
+        <a href="#" className="navbar-item is-tab" target="_blank">
+          <i className="fa-brands fa-github"></i>
+        </a>
+
+        <a href="#" className="navbar-item is-tab" target="_blank">
+          <i className="fa-brands fa-linkedin"></i>
+        </a>
+
+        <a href="#" className="navbar-item is-tab" target="_blank">
+          <i className="fa-solid fa-envelope"></i>
+        </a>
+
+        <a href="#" className="navbar-item is-tab" target="_blank">
+          <i className="fa-solid fa-phone"></i>
+        </a>
+        <a></a>
+      </div>
+    </div>
   )
 }
 
--- src/Components/MainScreen/IntroBody.jsx
+++ src/Components/MainScreen/IntroBody.jsx
@@ -1,12 +1,26 @@
 import React from 'react'
+import {useTypewriter,Cursor} from 'react-simple-typewriter'
 import '/src/css/introBody.css'
 
+
+
 function IntroBody() {
+
+    const [typeWords] = useTypewriter({ words: ["Saieswar Reddy Vaka", "a Machine Learning Enthusiast","a Software Developer", "an Open Source Contributor"], loop: {}, typeSpeed: 50, deleteSpeed: 50, });
   return (
     <div><div className="portfolio">
-    <text className="portfolio-title">Hello World</text>
-    <h2 className="portfolio-subtitle">I'm Sai</h2>
-  </div></div>
+    <text className="portfolio-title">Hi There!</text>
+    <text className="portfolio-subtitle">I'm <span className = "portfolio-subsubtitle">{typeWords}</span><Cursor></Cursor></text>
+    <text className="portfolio-title">based in San Jose, California. <br></br>saieswarreddy.vaka@sjsu.edu</text>
+  </div>
+  <div className="arrowContainer" >
+        <a href="#features"
+          ><i
+            class="fa-solid fa-circle-chevron-down fa-lg vert-move2 has-text-white"
+          ></i
+        ></a>
+      </div>
+    </div>
   )
 }
 
