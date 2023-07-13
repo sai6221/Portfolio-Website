0af7a71b38851087a0a01cfd17b1619adff1bf70
Sai Reddy <sai6221@gmail.com>
2023-07-12 21:20:30 -0700

    Added the description and aligned

--- src/Components/BodyScreen/Description.jsx
+++ src/Components/BodyScreen/Description.jsx
@@ -6,8 +6,7 @@ function Description() {
     <div className='descriptionContainer'>
         <button className='buttonResume'>Download Resume <i class="downloadButton fa-solid fa-download"></i></button>
         <p className='descriptionContent'>
-            Hey There!! I'm Saieswar Reddy Vaka (a.k.a Sai) and I am a CS graduate student at San Jose State University.
-</p>
+        Hey there! I am Saieswar Reddy Vaka (a.k.a Sai) and I am an MS CS student at San Jose State University. Being Machine Learning Enthusiast, I would love to contribute to products that touch millions of people around the globe. Exploring open source has completely evloved me into a better developer with hands on experience on current technologies. In the past, I worked as an iOS developer and a Machine Learning Engineer at a very fast paced start ups. Feel free to reach out if you like what you see, are working on interesting projects, or just wanna say hi!</p>
     </div>
   )
 }
