34734b388331e5a215f7ac53ec27535666c885e8
Sai Reddy <sai6221@gmail.com>
2023-07-13 17:29:21 -0700

    Skills Section Completed

--- src/Components/BodyScreen/Skills.jsx
+++ src/Components/BodyScreen/Skills.jsx
@@ -9,7 +9,32 @@ function Skills() {
         <h1 className='skills-heading'>Skills</h1>
 
         <div className='skills'>
-      <Skillcomponent imageUrl="\Me.jpg" name="Saieswar Reddy "></Skillcomponent>
+        <Skillcomponent imageUrl="\Python.png" name="Python"></Skillcomponent>
+        <Skillcomponent imageUrl="\Pytorch.png" name="Pytorch"></Skillcomponent>
+        <Skillcomponent imageUrl="\Tensorflow.png" name="Tensorflow"></Skillcomponent>
+        <Skillcomponent imageUrl="\Sql.png" name="Sql"></Skillcomponent>
+        <Skillcomponent imageUrl="\Scikit.png" name="Scikit"></Skillcomponent>
+        <Skillcomponent imageUrl="\Go.png" name="Go"></Skillcomponent>
+        <Skillcomponent imageUrl="\Rest.png" name="REST API"></Skillcomponent>
+        <Skillcomponent imageUrl="\JavaScript.png" name="JavaScript"></Skillcomponent>
+        <Skillcomponent imageUrl="\Java.png" name="Java"></Skillcomponent>
+        <Skillcomponent imageUrl="\Haskell.png" name="Haskell"></Skillcomponent>
+        <Skillcomponent imageUrl="\Git.png" name="Git"></Skillcomponent>
+        <Skillcomponent imageUrl="\React.png" name="ReactJS"></Skillcomponent>
+        <Skillcomponent imageUrl="\Html_CSS.png" name="Html/CSS"></Skillcomponent>
+        <Skillcomponent imageUrl="\AWS.png" name="AWS"></Skillcomponent>
+      <Skillcomponent imageUrl="\Node.png" name="NodeJS"></Skillcomponent>
+      <Skillcomponent imageUrl="\Angular.svg" name="AngularJS"></Skillcomponent>
+      <Skillcomponent imageUrl="\Docker.png" name="Docker"></Skillcomponent>
+      <Skillcomponent imageUrl="\Kubernetes.png" name="Kubernetes"></Skillcomponent>
+      <Skillcomponent imageUrl="\Figma2.png" name="Figma"></Skillcomponent>
+      <Skillcomponent imageUrl="\Firebase.png" name="Firebase "></Skillcomponent>
+      <Skillcomponent imageUrl="\Jira.png" name="Jira"></Skillcomponent>
+      <Skillcomponent imageUrl="\MongoDB.png" name="MongoDB"></Skillcomponent>
+      <Skillcomponent imageUrl="\Swift.svg" name="Swift"></Skillcomponent>
+      <Skillcomponent imageUrl="\Xcode.png" name="Xcode"></Skillcomponent>
+      <Skillcomponent imageUrl="\Android.png" name="Android"></Skillcomponent>
+      <Skillcomponent imageUrl="\Postgres.png" name="Postgres"></Skillcomponent>
       </div>
     </div>
   )
