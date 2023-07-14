901a4d40774543479ade891a419d28d1baa9f6f3
Sai Reddy <sai6221@gmail.com>
2023-07-14 16:06:34 -0700

    Contact Me

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
 import MainScreen from './Components/MainScreen/MainScreen'
 import BodyScreen from './Components/BodyScreen/BodyScreen'
+import ContactScreen from './Components/ContactScreen/ContactScreen'
 
 function App() {
     return (
     <div>
       <MainScreen></MainScreen>
         <BodyScreen></BodyScreen>
-        {/* <ContactScreen></ContactScreen> */}
+        <ContactScreen></ContactScreen>
   </div>
     )
 }
---
+++ src/Components/ContactScreen/ContactScreen.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import '/src/css/contactScreen.css'
+
+function ContactScreen() {
+  return (
+    <div className='contactContainer'>
+        <div className='contact-heading'>Contact Me</div>
+        </div>
+  )
+}
+
+export default ContactScreen
