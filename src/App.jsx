71b68c126d3fd4140d08439c6a95bbafb48513fd
Sai Reddy <sai6221@gmail.com>
2023-07-16 15:45:50 -0700

    fixes

--- src/App.jsx
+++ src/App.jsx
@@ -5,25 +5,12 @@ import ContactScreen from './Components/ContactScreen/ContactScreen'
 import { createBrowserRouter, RouterProvider, Link } from 'react-router-dom'
 
 function App() {
-  const router = createBrowserRouter(routes, { basename: import.meta.env.DEV ? '/' : '/' })
     return (
-<BrowserRouter
-      basename={import.meta.env.DEV ? '/' : '/'}
-    >
-      <Routes>
-        <Route
-          path='/'
-          element={
-            <div>
-<MainScreen></MainScreen>
-  <BodyScreen></BodyScreen>
-  <ContactScreen></ContactScreen>
-</div>
-          }
-        />
-        <Route path='/about' element={<div>About</div>} />
-      </Routes>
-    </BrowserRouter>
+    <div>
+      <MainScreen></MainScreen>
+        <BodyScreen></BodyScreen>
+        <ContactScreen></ContactScreen>
+  </div>
     )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from 'react';
+import App from './App';
+import './index.css';
+import { BrowserRouter } from 'react-router-dom';
+import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  ReactDOM.render(
-    <React.StrictMode>
-              <App />
-    </React.StrictMode>,
-    document.getElementById('root')
-  )
-)
+createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
