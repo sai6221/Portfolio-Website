4835fbd80529b5d6e15699da67a6bd32c53d0e62
Sai Reddy <sai6221@gmail.com>
2023-07-16 15:19:09 -0700

    fixes

--- src/main.jsx
+++ src/main.jsx
@@ -6,9 +6,7 @@ import './index.css'
 ReactDOM.createRoot(document.getElementById('root')).render(
   ReactDOM.render(
     <React.StrictMode>
-          <BrowserRouter basename={process.env.PUBLIC_URL}>
               <App />
-          </BrowserRouter>
     </React.StrictMode>,
     document.getElementById('root')
   )
