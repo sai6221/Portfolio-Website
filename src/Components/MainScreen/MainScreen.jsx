107b76a479cb3c5de7e9bab0bf1cf6e834ca7c18
Sai Reddy <sai6221@gmail.com>
2023-07-16 15:26:59 -0700

    fixes

--- src/Components/MainScreen/MainScreen.jsx
+++ src/Components/MainScreen/MainScreen.jsx
@@ -3,7 +3,7 @@ import Navbar from './Navbar'
 import IntroBody from './IntroBody'
 import FooterBody from './FooterBody'
 import '/src/css/Navbar.css';
-import '/src/css/MainScreen.css';
+import '/src/css/mainScreen.css';
 import '/src/css/introBody.css';
 
 function MainScreen() {
