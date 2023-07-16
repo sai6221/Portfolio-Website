95283317c47b829e5fc71bd0b85d1caeae6da277
Sai Reddy <sai6221@gmail.com>
2023-07-16 15:06:25 -0700

    Fixes

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,28 @@ import './App.css'
 import MainScreen from './Components/MainScreen/MainScreen'
 import BodyScreen from './Components/BodyScreen/BodyScreen'
 import ContactScreen from './Components/ContactScreen/ContactScreen'
+import { createBrowserRouter, RouterProvider, Link } from 'react-router-dom'
 
 function App() {
+  const router = createBrowserRouter(routes, { basename: import.meta.env.DEV ? '/' : '/react-vite-gh-pages/' })
     return (
-    <div>
-      <MainScreen></MainScreen>
-        <BodyScreen></BodyScreen>
-        <ContactScreen></ContactScreen>
-  </div>
+<BrowserRouter
+      basename={import.meta.env.DEV ? '/' : '/react-vite-gh-pages/'}
+    >
+      <Routes>
+        <Route
+          path='/'
+          element={
+            <div>
+<MainScreen></MainScreen>
+  <BodyScreen></BodyScreen>
+  <ContactScreen></ContactScreen>
+</div>
+          }
+        />
+        <Route path='/about' element={<div>About</div>} />
+      </Routes>
+    </BrowserRouter>
     )
 }
 
