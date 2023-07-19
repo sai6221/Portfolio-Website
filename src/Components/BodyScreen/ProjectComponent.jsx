28ee0952ef9294f1d69cfa5831002b166e4ca518
Sai Reddy <sai6221@gmail.com>
2023-07-18 18:31:55 -0700

    Projects Section Added.

--- src/Components/BodyScreen/ProjectComponent.jsx
+++ src/Components/BodyScreen/ProjectComponent.jsx
@@ -1,14 +1,15 @@
 import React from 'react'
 import '/src/css/projectsScreen.css'
 
-function ProjectComponent({ imageUrl, name }) {
+function ProjectComponent({ title, skills, description }) {
     return (
         <div className="project-component">
-          <div className="project-image">
-            <img src={imageUrl} alt={name} />
-          </div>
-          <div className="project-name">{name}</div>
-        </div>
+      <div className="project-content">
+        <div className="project-title">{title}</div>
+        <div className="project-Skills">{skills}</div>
+        <div className="project-description">{description}</div>
+      </div>
+    </div>
       );
 }
 
--- src/Components/BodyScreen/Projects.jsx
+++ src/Components/BodyScreen/Projects.jsx
@@ -7,7 +7,18 @@ function Projects() {
     <div className='projectsContainer' id='projectsContainer'>
         <h1 className='projects-heading'>Projects</h1>
         <div className='projects'>
-        <ProjectComponent imageUrl="Python.png" name="Python"></ProjectComponent>
+        <ProjectComponent title="Fake Malware Generation using GAN’s" skills="PyTorch, Python" description="Implemented generative adversarial networks (GANS) to generate adversarial malware samples as DLLs/API calls to
+differentiate between fake and benign samples."></ProjectComponent>
+
+<ProjectComponent title="BirdCall Identification" skills ="TensorFlow, Sci-kit, Keras, Python" description="Executed a robust solution to identify and extract useful features of birds’ voices from natural soundscapes using Short- time
+Fourier transform and Mel Frequency graphs as pre-processing techniques to classify them based on their species."></ProjectComponent>
+
+<ProjectComponent title="Peek ’N’ Shop" skills="JavaScript, Angular, MUI, Go" description="Pioneered a website for students at University of Florida to find affordable places and deals in and around campus leveraging
+Angular, JS and bootstrap framework devised using Figma."></ProjectComponent>
+{/* <ProjectComponent title="Fake Malware Generation using GAN’s" description="Implemented generative adversarial networks (GANS) to generate adversarial malware samples as DLLs/API calls to
+differentiate between fake and benign samples."></ProjectComponent>
+<ProjectComponent title="BirdCall Identification" description="Implemented generative adversarial networks (GANS) to generate adversarial malware samples as DLLs/API calls to
+differentiate between fake and benign samples."></ProjectComponent> */}
       </div>
     </div>
   )
