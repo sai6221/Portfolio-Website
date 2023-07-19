1252059a6f42c8bf05b11f929668484f787bfc69
Sai Reddy <sai6221@gmail.com>
2023-07-19 15:53:33 -0700

    Added all the components

--- src/Components/MainScreen/FooterBody.jsx
+++ src/Components/MainScreen/FooterBody.jsx
@@ -15,11 +15,11 @@ function FooterBody() {
           <i className="fa-brands fa-linkedin"></i>
         </a>
 
-        <a href="mailto:saieswarreddy.vaka@sjsu.edu" className="navbar-item is-tab" target="_blank">
+        <a href="mailto:saieswarreddy.vaka@sjsu.edu" className="navbar-item is-tab">
           <i className="fa-solid fa-envelope"></i>
         </a>
 
-        <a href="#" className="navbar-item is-tab" target="_blank">
+        <a href="+1-352-740-9880" className="navbar-item is-tab">
           <i className="fa-solid fa-phone"></i>
         </a>
         <a></a>
